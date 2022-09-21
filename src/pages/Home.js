fbaf077ecc09caaad4d0dcee863ee4c969021865
formal <rlaxodud9028@gmail.com>
2022-09-21 21:51:16 +0900

    making MovieSlide with react-multi-carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { movieAction } from '../redux/actions/MovieAction';
+import { useDispatch, useSelector } from 'react-redux';
+import Banner from "../components/Banner";
+import MovieSlide from '../components/MovieSlide';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const {popularMovies, topRatedMovies, upComingMovies} = useSelector(state => state.movie);
+
+  useEffect(() => {
+    dispatch(movieAction.getMovies());
+  },[]);
+
   return (
     <div>
-      Home
+      {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+      <h1>popular Movie</h1>
+      <MovieSlide movies={popularMovies} />
+      <h1>Top rated Movie</h1>
+      <MovieSlide movies={topRatedMovies} />
+      <h1>Upcoming Movie</h1>
+      <MovieSlide movies={upComingMovies} />
     </div>
   );
 };
---
+++ src/redux/actions/MovieAction.js
@@ -0,0 +1,32 @@
+import api from "../api";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+function getMovies() {
+  return async (dispatch) => {
+    const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+
+    const topRatedApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
+
+    const upComingApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
+    // let url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
+    // let response = await fetch(url);
+    // let data = await response.json();
+
+
+    //3개의 API호출 동시에 진행시키기
+    let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
+      popularMovieApi, 
+      topRatedApi, 
+      upComingApi
+    ]);
+    dispatch({
+      type: "GET_MOVIES_SUCCESS",
+      payload: { popularMovies: popularMovies.data, topRatedMovies: topRatedMovies.data, upComingMovies: upComingMovies.data },
+    })
+  }
+}
+
+export const movieAction = {
+  getMovies,
+}
